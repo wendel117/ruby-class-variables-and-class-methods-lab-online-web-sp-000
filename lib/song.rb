ba9f8b16d9870fe5efee0819fd3c0da304986e4c
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-26 15:40:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+
+def name
+  @name = name
+end
+
+def artist
+  @artist = artist
+end
+
+def genre
+  @genre = genre
+end
+
+def self.count
+end
+def self.artists
+end
+
+def self.genres
+end
+
+def self.genre_count
+end
+def self.artist_count
+end
+
+
+
+end
