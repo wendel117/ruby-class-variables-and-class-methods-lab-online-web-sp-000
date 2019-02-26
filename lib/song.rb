a2f24a8e1f43d7f5e7e7c545ef5ade880e641a64
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-26 16:10:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ class Song
   @@count = 0
     @@artists = []
     @@genres = []
-
+    @@genre_count = {}
+     @@artists_count = {}
 
 def initialize(name, artist, genre)
   @name = name
@@ -26,5 +27,12 @@ def self.artists
   @@artists.uniq
 end
 
+def self.genre_count
+
+end
+
+def self.artist_count
+
+end
 
 end
