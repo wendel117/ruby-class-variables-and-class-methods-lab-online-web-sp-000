72af8f518d8eb69681f1fd01ce614fe33928335f
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-26 15:46:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artist_count = 0
-  @@genre_count = 0
+#  @@song_count = 0
+#  @@artist_count = 0
+#  @@genre_count = 0
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
-  @@count += 1
+  @@song_count += 1
   @@artist_count += 1
   @@genre_count += 1
 end
 
 def self.count
-  @@count
+  @@song_count
 end
 
 def self.genre_count
